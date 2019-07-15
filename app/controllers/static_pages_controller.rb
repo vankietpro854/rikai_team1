ae17ce7ac4aae65147fa553d811f9d0c0ac703a4
VanHue <vanhue854@gmail.com>
2019-07-15 13:18:32 +0700

    update admin

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,28 @@
 class StaticPagesController < ApplicationController
+  before_action :logged_in_user, only: [:admin]
+  before_action :admin_user,     only: :admin
+
   def home
   end
 
   def help
   end
+
+  def admin
+    @users = User.all # not paginated
+    @users = User.paginate(page: params[:page]) # paginated
+  end
+
+  private
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
       if @user.save
-        log_in @user
         flash[:success] = "User was successfully updated"
         render 'show'
       else
